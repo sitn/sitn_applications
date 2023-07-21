62a939114010ae648a3d86e131b9b32e249e8092
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2023-07-21 09:07:41 +0200

    DB migrations to be able to upload old data

--- parcel_historisation/models.py
+++ parcel_historisation/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class State(models.Model):
     name = models.CharField(max_length=1000)
@@ -31,9 +32,10 @@ class Plan(models.Model):
         db_table = 'parcel_historisation\".\"plan'
 
 class Operation(models.Model):
-    date = models.DateField()
-    user = models.CharField(max_length=200, null=False)
-    complement = models.CharField(max_length=5000)
+    date = models.DateField(null=True)
+    user = models.CharField(max_length=200, null=True)
+    complement = models.CharField(max_length=5000, null=True)
+    old_system = models.BooleanField(default=False)
     plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, verbose_name='plan', blank=True, null=True)
 
     class Meta:
@@ -42,6 +44,7 @@ class Operation(models.Model):
 class OtherOperation(models.Model):
     type = models.IntegerField()
     operation = models.ForeignKey(Operation, on_delete=models.SET_NULL, verbose_name='operation', null=True)
+    bfs_list = ArrayField(models.CharField(max_length=200, null=True), null=True)
 
     class Meta:
         db_table = 'parcel_historisation\".\"other_operation'
@@ -55,11 +58,12 @@ class DivisonReunion(models.Model):
 class Balance(models.Model):
     source = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
-    source_rp = models.BooleanField()
-    destination_rp = models.BooleanField()
-    source_origin = models.BooleanField()
-    current_destination = models.BooleanField()
-    id_division = models.ForeignKey(Operation, on_delete=models.SET_NULL, verbose_name='id_division', null=True)
+    source_rp = models.BooleanField(null=True)
+    destination_rp = models.BooleanField(null=True)
+    source_origin = models.BooleanField(null=True)
+    current_destination = models.BooleanField(null=True)
+    division = models.ForeignKey(Operation, on_delete=models.SET_NULL, verbose_name='division', null=True)
+    destination_ddp = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'parcel_historisation\".\"balance'
