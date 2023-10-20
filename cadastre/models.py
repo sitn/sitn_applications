34703da22ea8bbb45018d684aa7e83b49254cc46
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2023-10-20 15:16:33 +0200

    Parcel search

--- cadastre/models.py
+++ cadastre/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+from django.conf import settings
+from django.contrib.gis.db import models
+from django.contrib.postgres.search import SearchVectorField
 
 class Cadastre(models.Model):
     idobj = models.UUIDField(primary_key=True)
@@ -9,4 +11,31 @@ class Cadastre(models.Model):
     comnom = models.CharField(max_length=50)
 
     class Meta:
+        managed = False
         db_table = 'general\".\"la02_cadastres'
+
+
+class ImmeublesAdressesSearch(models.Model):
+    noobj = models.IntegerField(primary_key=True)
+    numcom = models.IntegerField()
+    cadastre = models.CharField(max_length=100)
+    nummai = models.CharField(max_length=7)
+    idemai = models.CharField(max_length=50)
+    typimm = models.CharField(max_length=17)
+    mutcou = models.CharField(max_length=8)
+    mutmai = models.CharField(max_length=8)
+    srfmai = models.IntegerField()
+    valide = models.CharField(max_length=1)
+    datedt = models.DateField()
+    nufeco = models.CharField(max_length=5)
+    source = models.CharField(max_length=50)
+    url_terris = models.CharField(max_length=250)
+    adresse = models.CharField(max_length=250)
+    nom_localite = models.CharField(max_length=250)
+    search_immeuble = models.CharField(max_length=250)
+    geom = models.PolygonField(srid=settings.DEFAULT_SRID)
+    _ts = SearchVectorField()
+
+    class Meta:
+        managed = False
+        db_table = 'searchtables\".\"search_immeubles_adresses'
--- cadastre/views.py
+++ cadastre/views.py
@@ -1,10 +1,27 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from cadastre.models import Cadastre
+from cadastre.models import Cadastre, ImmeublesAdressesSearch
 
+import re
 
 def get_cadastre(request):
 
     cadastres = Cadastre.objects.values('numcad', 'cadnom').all().order_by('cadnom')
 
     return JsonResponse(list(cadastres), safe=False)
+
+def search_parcel(request):
+    """
+      Extended properties on search estates by address.
+      This is an adapted and simplified copy from here:
+      https://github.com/camptocamp/c2cgeoportal/blob/2.6/geoportal/c2cgeoportal_geoportal/views/fulltextsearch.py
+    """
+
+    IGNORED_CHARS_RE = re.compile(r"[()&|!:<>\t]")
+    IGNORED_STARTUP_CHARS_RE = re.compile(r"^[']*")
+
+    settings = request.registry.settings.get("fulltextsearch", {})
+#    fts_normaliser = Normalize(settings)  
+
+
+    return {}
