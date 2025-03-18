df6d58adb2fd13d471e406c1a4a3d9bee755708b
voisardf <francois.voisard@ne.ch>
2025-03-18 18:06:03 +0100

    conflicts

---
+++ ppe/static/js/utils.js
@@ -0,0 +1,44 @@
+function showMap() {
+    // Get the checkbox
+    const checkBox = document.getElementById("check_mutation");
+    // Get the output text
+    const carte = document.getElementById("mapwidget");
+
+    // If the checkbox is checked, display the output text
+    mapwidget.style.display =  checkBox.checked ? "block" : "none";
+}
+
+function showSituationBatiments() {
+    // Get the selected value
+    const dossier_type = document.getElementById("type_dossier").value;
+
+    if (dossier_type == ""){
+        submit_btn.style.display = "none";
+        type_modification.style.display = "none";
+        type_constitution.style.display = "none";
+    }
+    else if (dossier_type == "C"){
+        submit_btn.style.display = "none";
+        type_modification.style.display = "none";
+        type_constitution.style.display = "block";
+    }
+    else {
+        type_modification.style.display = "block";
+        type_constitution.style.display = "none";
+        submit_btn.style.display = "block";
+    }
+}
+
+function showRefGeoshop() {
+    // Get the selected radio button
+    const situtation_bati = document.getElementsByName("situation_bati");
+
+    for (let i=0;i < situtation_bati.length; i++) {
+            if (situtation_bati[i].checked) {
+                choix_situation_bati = situtation_bati[i].value;
+                ref_geoshop.style.display = (choix_situation_bati == 'nouveau_batiment') ? "block" : "none";
+                submit_btn.style.display = "block";
+            }
+
+        }
+    }
