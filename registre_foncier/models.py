715cb7677cb02906ce8819c331b4f0b8ad8f3f79
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2026-02-13 15:15:51 +0100

    Add view to retrieve parcel parents

--- registre_foncier/models.py
+++ registre_foncier/models.py
@@ -18,3 +18,27 @@ class RfcenAdresse(models.Model):
     class Meta:
         db_table = 'RFCEN\".\"ADRESSE'
         managed = False
+
+
+class Recht(models.Model):
+    """
+    Global table containing all needed information concerning
+    owners and people referenced in the RF DB.
+    akt_ase_typ enables to check if it is a moral or physcial
+    person or even some kind of wonership group (hoirie, société simple,
+    indivision familiale).
+    """
+
+    rht_id = models.CharField(max_length=10, primary_key=True, null=False)
+    lst_id = models.CharField(max_length=8)
+    lst_typ = models.CharField(max_length=1)
+    gdk_last = models.CharField(max_length=12)
+    esta_last = models.CharField(max_length=4)
+    art_last = models.CharField(max_length=1)
+    gdk_recht = models.CharField(max_length=12)
+    esta_recht = models.CharField(max_length=4)
+    loesch_status = models.CharField(max_length=1)
+
+    class Meta:
+        db_table = 'RF_SITN\".\"RECHT'
+        managed = False
