e656c064e1454336e3f6f836cf5bacf2dd80244e
maltaesousa <stephane.maltaesousa@ne.ch>
2026-02-16 17:12:09 +0100

    fix urls on prod and prepub

--- sitn/urls.py
+++ sitn/urls.py
@@ -21,24 +21,23 @@ from sitn.router import SitnRouter
 
 router = SitnRouter()
 
-
-router.register_app('cadastre', '/cadastre/')
-router.register_app('registre_foncier', '/registre_foncier/')
+router.register_app('cadastre', 'cadastre/')
+router.register_app('registre_foncier', 'registre_foncier/')
 urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('', router.get_api_root_view(), name='api-root'),
-    path('cadastre/', include('cadastre.urls')),
+    path('cadastre/', include('cadastre.urls', namespace='cadastre')),
     path('registre_foncier/', include('registre_foncier.urls')),
 ]
 
 if settings.IS_INTRANET:
-    router.register_app('ecap_intra', '/ecap/')
-    router.register_app('cats', '/cats/')
-    router.register_app('parcel_historisation', '/parcel_historisation/')
-    router.register_app('intranet_proxy', '/intranet_proxy/')
+    router.register_app('ecap_intra', 'ecap/')
+    router.register_app('cats', 'cats/')
+    router.register_app('parcel_historisation', 'parcel_historisation/')
+    router.register_app('intranet_proxy', 'intranet_proxy/')
     urlpatterns.extend([
         path('ecap/', include('ecap_intra.urls')),
         path('cats/', include('cats.urls')),
@@ -46,12 +45,12 @@ if settings.IS_INTRANET:
         re_path(r'(?:vcron|intranet)_proxy/', include('intranet_proxy.urls')),
     ])
 else:
-    router.register_app('ecap', '/ecap/')
-    router.register_app('action_sociale', '/action_sociale/')
-    router.register_app('health', '/health/')
-    router.register_app('roads', '/roads/')
-    router.register_app('stationnement', '/stationnement/')
-    router.register_app('forest_forpriv', '/forest_forpriv/')
+    router.register_app('ecap', 'ecap/')
+    router.register_app('action_sociale', 'action_sociale/')
+    router.register_app('health', 'health/')
+    router.register_app('roads', 'roads/')
+    router.register_app('stationnement', 'stationnement/')
+    router.register_app('forest_forpriv', 'forest_forpriv/')
     urlpatterns.extend([
         path('ecap/', include('ecap.urls')),
         path('action_sociale/', include('action_sociale.urls')),
